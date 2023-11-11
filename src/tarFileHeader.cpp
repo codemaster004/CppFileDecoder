1b3b387c51b9ebdbdfe24d567dde4c934f15e800
Filip Dabkowski <filip.dabkowski@gmail.com>
2023-11-11 12:29:54 +0100

    create class to hold data

---
+++ src/tarFileHeader.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Filip Dabkowski on 09/11/2023.
+//
+
+#include "tarFileHeader.h"
+
+void tarFileHeader::appendBytes(const char bytes[], int count) {
+
+}
---
+++ src/tarFileHeader.h
@@ -0,0 +1,16 @@
+class tarFileHeader {
+private:
+    char fileName[100];
+    char fileMode[8];
+    char ownerId[8];
+    char groupId[8];
+    unsigned long long int fileSize;
+    char modificationDate[12];
+
+    int importedBytesCount;
+
+public:
+    void appendBytes(const char bytes[], int count);
+};
+
+
